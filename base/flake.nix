aaf7507c842a5703b2292c921e4cda44511ed54f
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2022-08-27 14:29:13 +0200

    base: remove overlay, fix inputs

--- base/flake.nix
+++ base/flake.nix
@@ -1,31 +1,38 @@
 {
-  nixConfig.bash-prompt-suffix = "(nix) ";
+  nixConfig.bash-prompt-prefix = "(nix) ";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  # it is recommended to pin to a specific revision or tag of nixpkgs
+  # e.g.:
+  # inputs.nixpkgs.url = "github:nixos/nixpkgs/22.05"
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/22.05";
+
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    # minimize how many nixpkgs we evaluate
+    # might hit cache less
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   inputs.flake-compat = {
     url = github:edolstra/flake-compat;
+    # minimize how many nixpkgs we evaluate
+    # might hit cache less
+    inputs.nixpkgs.follows = "nixpkgs";
     flake = false;
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    let
-      overlay = import ./nix/overlay.nix;
-    in
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ overlay ];
-          };
+          # don't use overlays here
+          # https://zimbatm.com/notes/1000-instances-of-nixpkgs
+          pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          devShell = pkgs.callPackage ./nix/devShell.nix { };
+          devShells.default = pkgs.callPackage ./nix/devShell.nix { };
 
+          # useful for debugging with the Nix REPL
           passthru = { inherit pkgs; };
         }
-      )
-    // {
-      inherit overlay;
-    };
+      );
 }
